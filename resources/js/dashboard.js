e180f177b6efc277c3682b9107bf9c42d3902a3f
Gowtham602 <gowthamsundararajan7777@gmail.com>
2026-04-03 10:32:16 +0530

    js changes

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -38,12 +38,6 @@
 
 // });
 
-
-
-
-
-
-
 // // document.addEventListener('DOMContentLoaded', function () {
 
 // //     // DataTable
@@ -127,18 +121,15 @@
 
 // // });
 
-
-
-
 // import './bootstrap';
 console.log("1233");
 let table;
-let filePath = '';
+let filePath = "";
 $(document).ready(function () {
-console.log("hh231");
+    console.log("hh231");
 
     function addRow() {
-        $('#imageContainer').append(`
+        $("#imageContainer").append(`
             <div class="upload-box d-flex gap-2">
                 <input type="file" name="images[]" class="form-control" required>
                 <button type="button" class="btn btn-danger remove-btn">Remove</button>
@@ -151,19 +142,19 @@ console.log("hh231");
     }
 
     // Delegate remove click
-    $(document).on('click', '.remove-btn', function () {
+    $(document).on("click", ".remove-btn", function () {
         removeRow(this);
     });
 
     window.addRow = addRow; // expose globally
 
     // FORM SUBMIT
-    $('#mergeForm').submit(function(e) {
+    $("#mergeForm").submit(function (e) {
         e.preventDefault();
 
         let formData = new FormData(this);
 
-        $('#loader').css('display','flex');
+        $("#loader").css("display", "flex");
 
         $.ajax({
             url: "/process-images",
@@ -172,101 +163,60 @@ console.log("hh231");
             contentType: false,
             processData: false,
             headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
             },
+            success: function (res) {
+                $("#loader").hide();
 
-//             success: function(res) {
-//                     $('#shortUrl').html(
-//     'Short URL: <a href="' + res.short_url + '" target="_blank">' + res.short_url + '</a>'
-// );
-//                 $('#loader').hide();
+                const url = res.image + "?t=" + new Date().getTime();
+                filePath = res.file_path;
 
-//                 const url = res.image + '?t=' + new Date().getTime();
+                $("#modalImage").attr("src", url);
 
-//                 $('#modalImage').attr('src', url);
+                //  CLEAR FILE INPUTS
+                $("#mergeForm")[0].reset();
 
-//                 $('#modalDownload').off('click').on('click', function () {
-//                     const a = document.createElement('a');
-//                     a.href = url;
-//                     a.download = 'merged.jpg';
-//                     a.click();
-//                 });
+                $("#modalDownload")
+                    .off("click")
+                    .on("click", function () {
+                        const a = document.createElement("a");
+                        a.href = url;
+                        a.download = "merged.jpg";
+                        a.click();
+                    });
 
-//                 let modal = new bootstrap.Modal(document.getElementById('resultModal'));
-//                 modal.show();
-//             },
-// success: function(res) {
-
-//     $('#loader').hide();
-
-//     const url = res.image + '?t=' + new Date().getTime();
-//       filePath = res.file_path;
-//     $('#modalImage').attr('src', url);
-
-//     //  SHOW SHORT URL
-//     // $('#shortUrl').html(
-//     //     'Short URL: <a href="' + res.short_url + '" target="_blank">' + res.short_url + '</a>'
-//     // );
-
-//     $('#modalDownload').off('click').on('click', function () {
-//         const a = document.createElement('a');
-//         a.href = url;
-//         a.download = 'merged.jpg';
-//         a.click();
-//     });
-
-//     let modal = new bootstrap.Modal(document.getElementById('resultModal'));
-//     modal.show();
-// },
-success: function(res) {
-
-    $('#loader').hide();
-
-    const url = res.image + '?t=' + new Date().getTime();
-    filePath = res.file_path;
-
-    $('#modalImage').attr('src', url);
-
-    //  CLEAR FILE INPUTS
-    $('#mergeForm')[0].reset();
-
-     $('#modalDownload').off('click').on('click', function () {
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'merged.jpg';
-        a.click();
-    });
-
-    //  REMOVE EXTRA ADDED INPUT ROWS (keep only one)
-    $('#imageContainer').html(`
+                //  REMOVE EXTRA ADDED INPUT ROWS (keep only one)
+                $("#imageContainer").html(`
         <div class="upload-box d-flex gap-2">
             <input type="file" name="images[]" class="form-control" required>
             <button type="button" class="btn btn-danger remove-btn">Remove</button>
         </div>
     `);
 
-    let modal = new bootstrap.Modal(document.getElementById('resultModal'));
-    modal.show();
-},
+                let modal = new bootstrap.Modal(
+                    document.getElementById("resultModal"),
+                );
+                modal.show();
+            },
 
-            error: function(xhr) {
-                $('#loader').hide();
+            error: function (xhr) {
+                $("#loader").hide();
                 console.log(xhr.responseText);
                 alert("Error! Check console");
-            }
+            },
         });
     });
 
     // MODE CHANGE
-    $('select[name="mode"]').on('change', function () {
+    $('select[name="mode"]')
+        .on("change", function () {
+            $("#finalImage").attr("src", "");
 
-        $('#finalImage').attr('src', '');
+            let mode = $(this).val();
+            let html = "";
 
-        let mode = $(this).val();
-        let html = '';
-
-        if (mode === 'vertical') {
-            html = `
+            if (mode === "vertical") {
+                html = `
                 <div class="row">
                     <div class="col-md-4">
                         <label>Width (px)</label>
@@ -282,8 +232,8 @@ success: function(res) {
                     </div>
                 </div>
             `;
-        } else {
-            html = `
+            } else {
+                html = `
                 <div class="row">
                     <div class="col-md-4">
                         <label>Height (px)</label>
@@ -299,217 +249,108 @@ success: function(res) {
                     </div>
                 </div>
             `;
-        }
-
-        $('#dynamicOptions').html(html);
-
-    }).trigger('change');
-
-});
-
-$('#saveBtn').off('click').on('click', function () {
-
-    let imageName = $('#renameInput').val();
-        console.log(imageName,"___name");
-    if (!imageName) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Enter image name'
-        });
-        return;
-    }
-
-    if (!filePath) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Generate image first'
-        });
-        return;
-    }
-
-    $.ajax({
-        url: "/save-image",
-        type: "POST",
-        data: {
-            image_name: imageName,
-            file_path: filePath,
-            _token: $('meta[name="csrf-token"]').attr('content')
-        },
-
-        // success: function(res) {
-
-        //     //  SUCCESS ALERT
-        //     Swal.fire({
-        //         icon: 'success',
-        //         title: 'Success',
-        //         text: res.message
-        //     });
-
-        //     //  SHOW SHORT URL
-        //     $('#').html(
-        //         'Short URL: <a href="' + res.short_url + '" target="_blank">' + res.short_url + '</a>'
-        //     );
-        // },
-//   success: function(res) {
-
-//     Swal.fire({
-//         icon: 'success',
-//         title: 'Success',
-//         text: res.message,
-//         timer: 1500,
-//         showConfirmButton: false
-//     }).then(() => {
-
-//         //  CLOSE MODAL AFTER ALERT
-//         let modalEl = document.getElementById('resultModal');
-//         let modal = bootstrap.Modal.getInstance(modalEl);
-
-//         if (modal) {
-//             modal.hide();
-//         }
-
-//         //  CLEAR INPUT
-//         $('#renameInput').val('');
-
-//         //  REFRESH TABLE
-//         loadImages();
-//     });
-
-// },
-
-success: function(res) {
-    console.log(res);
-    console.log("success message  ");
-
-
-    Swal.fire({
-        icon: 'success',
-        title: 'Success',
-        text: res.message,
-        timer: 1500,
-        showConfirmButton: false
-    }).then(() => {
-
-        // close modal
-        let modalEl = document.getElementById('resultModal');
-        let modal = bootstrap.Modal.getInstance(modalEl);
-        if (modal) modal.hide();
-
-        // clear input
-        $('#renameInput').val('');
-
-        //  RELOAD TABLE (IMPORTANT)
-        table.ajax.reload(null, false); 
-    });
-},
-        error: function(xhr) {
-
-            let msg = 'Something went wrong';
-
-            if (xhr.responseJSON && xhr.responseJSON.message) {
-                msg = xhr.responseJSON.message;
             }
 
-            //  ERROR ALERT
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: msg
-            });
-        }
-    });
+            $("#dynamicOptions").html(html);
+        })
+        .trigger("change");
 });
 
+$("#saveBtn")
+    .off("click")
+    .on("click", function () {
+        let imageName = $("#renameInput").val();
+        console.log(imageName, "___name");
+        if (!imageName) {
+            Swal.fire({
+                icon: "error",
+                title: "Enter image name",
+            });
+            return;
+        }
 
-// function loadImages() {
+        if (!filePath) {
+            Swal.fire({
+                icon: "error",
+                title: "Generate image first",
+            });
+            return;
+        }
 
-//     let table = $('#imageTable').DataTable();
+        $.ajax({
+            url: "/save-image",
+            type: "POST",
+            data: {
+                image_name: imageName,
+                file_path: filePath,
+                _token: $('meta[name="csrf-token"]').attr("content"),
+            },
 
-//     $.ajax({
-//         url: "/get-images",
-//         type: "GET",
-//         success: function(res) {
+         
+            success: function (res) {
+                console.log(res);
+                console.log("success message  ");
 
-//             table.clear();
+                Swal.fire({
+                    icon: "success",
+                    title: "Success",
+                    text: res.message,
+                    timer: 1500,
+                    showConfirmButton: false,
+                }).then(() => {
+                    // close modal
+                    let modalEl = document.getElementById("resultModal");
+                    let modal = bootstrap.Modal.getInstance(modalEl);
+                    if (modal) modal.hide();
 
-//             res.images.forEach((img, index) => {
+                    // clear input
+                    $("#renameInput").val("");
 
-//                 let fullUrl = `${window.location.origin}/s/${img.short_code}`;
+                    //  RELOAD TABLE (IMPORTANT)
+                    table.ajax.reload(null, false);
+                });
+            },
+            error: function (xhr) {
+                let msg = "Something went wrong";
 
-//                 // 🔥 FIX FILE PATH HERE
-//                 let filePath = img.file_path;
+                if (xhr.responseJSON && xhr.responseJSON.message) {
+                    msg = xhr.responseJSON.message;
+                }
 
-//                 if (!filePath.startsWith('images/')) {
-//                     filePath = 'images/' + filePath;
-//                 }
+                //  ERROR ALERT
+                Swal.fire({
+                    icon: "error",
+                    title: "Error",
+                    text: msg,
+                });
+            },
+        });
+    });
 
-//                 // add extension if missing
-//                 if (!filePath.endsWith('.jpg') && !filePath.endsWith('.jpeg')) {
-//                     filePath = filePath + '.jpg';
-//                 }
-
-//                 let imageUrl = `${window.location.origin}/storage/${filePath}`;
-
-//                 table.row.add([
-//                     index + 1,
-
-//                     // IMAGE LINK  FIXED
-//                     `<a href="${imageUrl}" target="_blank">
-//                         ${img.image_name}.jpeg
-//                     </a>`,
-
-//                     // SHORT URL + COPY
-//                     `<div class="d-flex align-items-center gap-2">
-//                         <a href="${fullUrl}" target="_blank">
-//                             ${fullUrl}
-//                         </a>
-//                         <button class="btn btn-sm btn-light border rounded-3"
-//                             onclick="copyLink('${fullUrl}')">
-//                             <i class="bi bi-clipboard"></i>
-//                         </button>
-//                     </div>`,
-
-//                     img.click_count,
-
-//                     new Date(img.created_at).toLocaleString()
-//                 ]);
-
-//             });
-
-//             table.draw();
-//         }
-//     });
-// }
 window.copyLink = function (link) {
     navigator.clipboard.writeText(link).then(() => {
         Swal.fire({
-            icon: 'success',
-            title: 'Copied',
-            text: 'Short URL copied',
+            icon: "success",
+            title: "Copied",
+            text: "Short URL copied",
             timer: 1200,
-            showConfirmButton: false
+            showConfirmButton: false,
         });
     });
 };
 
-
-
-
 $(document).ready(function () {
-
-    table =  $('#imageTable').DataTable({
+    table = $("#imageTable").DataTable({
         processing: true,
         serverSide: true,
-        ajax: '/get-images',
+        ajax: "/get-images",
 
         columns: [
             { title: "#" },
             { title: "Image" },
             { title: "Short URL" },
             { title: "Count" },
-            { title: "Date" }
-        ]
-        
+            { title: "Date" },
+        ],
     });
-
 });
